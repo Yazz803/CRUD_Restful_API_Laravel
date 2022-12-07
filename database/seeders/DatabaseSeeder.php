1df5849c7aca6420861bad76029046ebda330c1f
Yazz803 <yazidakbar08@gmail.com>
2022-12-07 11:02:10 +0700

    first commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Post;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // \App\Models\User::factory(10)->create();
+
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+
+        // Post::create([
+        //     'image' => 'https://picsum.photos/200/300',
+        //     'title' => 'Post 1',
+        //     'content' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.',
+        // ]);
+
+        // Post::create([
+        //     'image' => 'https://picsum.photos/200/300',
+        //     'title' => 'Post 2',
+        //     'content' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.',
+        // ]);
+    }
+}
